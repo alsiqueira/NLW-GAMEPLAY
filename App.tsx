5a622ec2b09dae9c4e5a4559999267a960991079
Alexandre Luis Siqueira <alexandre@Mac-Pro-de-Alexandre.local>
2021-06-21 16:37:53 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import SignIn from './src/screens/SignIn';
 
-export default function App() {
+// import { Container } from './styles';
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SignIn />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Title>
+        Alexandre Siqueira
+      </Title>
+    </Container>
+  )
+}
+
+export default SignIn;
